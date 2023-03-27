88272f594fa31a2abaa40b318e725ed280ec707c
tuank288 <tuank2882001@gmail.com>
2023-03-27 17:07:47 +0700

    error

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr/public_api';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { USER_LOGIN_URL } from '../shared/constans/urls';
+import { IUserLogin } from '../shared/interfaces/IUserLogin';
+import { User } from '../shared/models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  private userSubject = new BehaviorSubject<User>(new User());
+  public userObservable:Observable<User>;
+  constructor(private http:HttpClient, private toastrService:ToastrService) {
+    this.userObservable = this.userSubject.asObservable();
+   }
+
+  login(userLogin:IUserLogin):Observable<User>{
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {
+          this.userSubject.next(user);
+          this.toastrService.success(
+            `Wellcome to Food ${user.name}`,
+            'Login Successful'
+          )
+        },
+        error: (errorResponse) => {
+           this.toastrService.error(errorResponse.error, 'Login Failed')
+        }
+      })
+    )
+  }
+}
