46050b80487d2e70ee3727edd88d880876c99180
tuank288 <tuank2882001@gmail.com>
2023-05-23 22:33:44 +0700

    update

--- frontend/public/src/app/shared/interfaces/IUserRegister.ts
+++
@@ -1,8 +0,0 @@
-export interface IUserRegister{
-    full_name : string;
-    email : string;
-    phone_number: number;
-    password : string;
-    confirmPassword : string;
-    address: string;
-  }
--- frontend/public/src/app/shared/models/Tag.ts
+++
@@ -1,8 +0,0 @@
-export class Tag{
-    category_id!: string;
-    category_image!:string;
-    category_name!: string;
-    created_at!: string;
-    updated_at!: string;
-    stt!:number;
-}
